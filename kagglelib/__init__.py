8ae12500265a0ee99dd9066551b015b648263be7
Panos Mavrogiorgos <pmav99@gmail.com>
2020-12-28 21:51:50 +0200

    refactor: Move third party data loaders to a separate module

--- kagglelib/__init__.py
+++ kagglelib/__init__.py
@@ -15,6 +15,16 @@ from holoviews import opts as hv_opts
 from .paths import DATA
 from .plots import hv_plot_value_count_comparison
 from .plots import sns_plot_value_count_comparison
+from .third_party import load_eurostat_df
+from .third_party import get_usd_eur_rate
+from .third_party import load_world_bank_groups
+from .third_party import load_eurostat_df
+from .third_party import load_oecd_df
+from .third_party import load_numbeo_df
+from .third_party import load_ilo_df
+from .third_party import load_mean_salary_comparison_df
+
+
 
 
 # Dictionaries of useful CONSTANTS
@@ -81,86 +91,6 @@ _KAGGLE_RENAMES = {
 }
 
 
-@functools.lru_cache(maxsize=1)
-def load_usd_eur_df() -> pd.DataFrame:
-    df = pd.read_csv(
-        DATA / "ecb_usd_euro_avg_exch_rate_filtered.csv",
-        header=None,
-        skiprows=5,
-        names=["year", "rate"],
-        index_col="year",
-    )
-    return df
-
-
-@functools.lru_cache(maxsize=1)
-def get_usd_eur_rate(year: int) -> Union[int, str, float]:
-    df = load_usd_eur_df()
-    rate = df.at[year, "rate"]
-    return rate
-
-@functools.lru_cache(maxsize=1)
-def load_world_bank_groups() -> pd.DataFrame:
-    df = pd.read_csv(
-        DATA / "wb_country_income_groups.csv",
-        header=0,
-        names=["country", "income_group"],
-        index_col="country",
-    )
-    df.income_group =df.income_group.str.replace(" income", "")
-    return df
-
-@functools.lru_cache(maxsize=1)
-def load_eurostat_df() -> pd.DataFrame:
-    usd_eur = get_usd_eur_rate(2019)
-    df = pd.read_csv(
-        DATA / "eurostat_gross_earnings_euros_2019_filtered.csv",
-        header=0,
-        names=["country", "eurostat"],
-        index_col="country",
-    )
-    df = df.assign(year=2019, eurostat=df.eurostat * usd_eur)
-    return df
-
-
-@functools.lru_cache(maxsize=1)
-def load_oecd_df() -> pd.DataFrame:
-    df = pd.read_csv(
-        DATA / "oecd_ann_avg_wage_2019.csv",
-        header=0,
-        names=["country", "oecd"],
-        index_col="country",
-    )
-    df = df.assign(year=2019)
-    return df
-
-
-@functools.lru_cache(maxsize=1)
-def load_numbeo_df() -> pd.DataFrame:
-    df = pd.read_csv(
-        DATA / "numbeo.csv",
-        header=0,
-        names=["country", "numbeo"],
-        index_col="country",
-    )
-    df.numbeo = df.numbeo * 12
-    df = df.assign(year=2020)
-    return df
-
-
-@functools.lru_cache(maxsize=1)
-def load_ilo_df() -> pd.DataFrame:
-    df = pd.read_csv(
-        DATA / "ilo_mean_nom_wage_usd.csv",
-        header=0,
-        usecols=[0, 5, 6],
-        names=["country", "year", "monthly"],
-    )
-    df = df.groupby("country").tail(1)
-    df = df.assign(ilo=df.monthly * 12)
-    df = df.drop(columns="monthly")
-    df = df.set_index("country")
-    return df
 
 
 @functools.lru_cache(maxsize=1)
@@ -178,22 +108,6 @@ def load_questions_df() -> pd.DataFrame:
     orig = load_orig_kaggle_df()
     questions_df = orig.loc[0].reset_index(drop=True)
     return questions_df
-
-
-@functools.lru_cache(maxsize=1)
-def load_mean_salary_comparison_df():
-    income_group = load_world_bank_groups()
-    eurostat = load_eurostat_df()
-    oecd = load_oecd_df()
-    ilo = load_ilo_df()
-    numbeo = load_numbeo_df()
-    df = pd.concat(
-        [income_group.income_group, eurostat.eurostat, oecd.oecd, ilo.ilo, numbeo.numbeo], axis="columns"
-    )
-    df.index.name = "country"
-    df = df.assign(avg_salary=df.bfill(axis=1).iloc[:, 1])
-    df = df.reset_index(drop=False)
-    return df
 
 
 def get_threshold(value: float, offset: int = 1):
---
+++ kagglelib/third_party.py
@@ -0,0 +1,105 @@
+import functools
+
+from typing import Union
+
+import pandas as pd
+
+from .paths import DATA
+
+
+@functools.lru_cache(maxsize=1)
+def load_usd_eur_df() -> pd.DataFrame:
+    df = pd.read_csv(
+        DATA / "ecb_usd_euro_avg_exch_rate_filtered.csv",
+        header=None,
+        skiprows=5,
+        names=["year", "rate"],
+        index_col="year",
+    )
+    return df
+
+
+@functools.lru_cache(maxsize=1)
+def get_usd_eur_rate(year: int) -> Union[int, str, float]:
+    df = load_usd_eur_df()
+    rate = df.at[year, "rate"]
+    return rate
+
+
+@functools.lru_cache(maxsize=1)
+def load_world_bank_groups() -> pd.DataFrame:
+    df = pd.read_csv(
+        DATA / "wb_country_income_groups.csv",
+        header=0,
+        names=["country", "income_group"],
+        index_col="country",
+    )
+    df.income_group = df.income_group.str.replace(" income", "")
+    return df
+
+
+@functools.lru_cache(maxsize=1)
+def load_eurostat_df() -> pd.DataFrame:
+    usd_eur = get_usd_eur_rate(2019)
+    df = pd.read_csv(
+        DATA / "eurostat_gross_earnings_euros_2019_filtered.csv",
+        header=0,
+        names=["country", "eurostat"],
+        index_col="country",
+    )
+    df = df.assign(year=2019, eurostat=df.eurostat * usd_eur)
+    return df
+
+
+@functools.lru_cache(maxsize=1)
+def load_oecd_df() -> pd.DataFrame:
+    df = pd.read_csv(
+        DATA / "oecd_ann_avg_wage_2019.csv",
+        header=0,
+        names=["country", "oecd"],
+        index_col="country",
+    )
+    df = df.assign(year=2019)
+    return df
+
+
+@functools.lru_cache(maxsize=1)
+def load_numbeo_df() -> pd.DataFrame:
+    df = pd.read_csv(
+        DATA / "numbeo.csv",
+        header=0,
+        names=["country", "numbeo"],
+        index_col="country",
+    )
+    df.numbeo = df.numbeo * 12
+    df = df.assign(year=2020)
+    return df
+
+
+@functools.lru_cache(maxsize=1)
+def load_ilo_df() -> pd.DataFrame:
+    df = pd.read_csv(
+        DATA / "ilo_mean_nom_wage_usd.csv",
+        header=0,
+        usecols=[0, 5, 6],
+        names=["country", "year", "monthly"],
+    )
+    df = df.groupby("country").tail(1)
+    df = df.assign(ilo=df.monthly * 12)
+    df = df.drop(columns="monthly")
+    df = df.set_index("country")
+    return df
+
+
+@functools.lru_cache(maxsize=1)
+def load_mean_salary_comparison_df():
+    income_group = load_world_bank_groups()
+    eurostat = load_eurostat_df()
+    oecd = load_oecd_df()
+    ilo = load_ilo_df()
+    numbeo = load_numbeo_df()
+    df = pd.concat([income_group.income_group, eurostat.eurostat, oecd.oecd, ilo.ilo, numbeo.numbeo], axis="columns")
+    df.index.name = "country"
+    df = df.assign(avg_salary=df.bfill(axis=1).iloc[:, 1])
+    df = df.reset_index(drop=False)
+    return df
