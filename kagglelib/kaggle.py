8e1ff7f7d1b826395cf9a93370d0c002d10505ea
Panos Mavrogiorgos <pmav99@gmail.com>
2021-01-01 10:23:17 +0200

    kaggle: Simplify CODE/ML XP levels
    We now use only 3 levels with the same age thresholds.

--- kagglelib/kaggle.py
+++ kagglelib/kaggle.py
@@ -68,8 +68,8 @@ SALARY_AGGREGATE_BINS = {
 }
 
 CODE_EXP_LEVELS={
-    "0": "0. no XP",
-    "0-1": "0. no XP",
+    "0": "1. low XP",
+    "0-1": "1. low XP",
     "1-2": "1. low XP",
     "3-5": "2. med XP",
     "5-10": "2. med XP",
@@ -78,13 +78,13 @@ CODE_EXP_LEVELS={
 }
 
 ML_EXP_LEVELS={
-    "0": "0. no XP",
-    "0-1": "0. no XP",
+    "0": "1. low XP",
+    "0-1": "1. low XP",
     "1-2": "1. low XP",
     "2-3": "1. low XP",
     "3-4": "2. med XP",
     "4-5": "2. med XP",
-    "5-10": "3. high XP",
+    "5-10": "2. med XP",
     "10-20": "3. high XP",
     "20+": "3. high XP",
 }
